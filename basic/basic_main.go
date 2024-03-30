b9777254464705cb072ae10aac0e134e3e8e0372
devkiran <52634537+devkiran2018@users.noreply.github.com>
2024-03-30 14:34:26 -0400

    initial commit

---
+++ basic/basic_main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"  // format package for all print functionality.
+)
+
+// where to start the program? where is the entry point? -> starts from mian() function
+// this is online comment
+
+/* multiline comment 
+this is comment on second line.
+ */
+
+func main() {
+	var variableName1 = "Go data types"
+
+	fmt.Printf("this will print %T \n", variableName1)
+}
